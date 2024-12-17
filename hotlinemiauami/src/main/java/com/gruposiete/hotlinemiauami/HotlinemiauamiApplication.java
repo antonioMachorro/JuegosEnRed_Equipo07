402263bfc421161310236ed00b3e61dccf756c7f
antonioMachorro <a01782114@tec.mx>
2024-12-17 20:40:30 +0100

    Implementacion WebSockets

--- hotlinemiauami/src/main/java/com/gruposiete/hotlinemiauami/HotlinemiauamiApplication.java
+++ hotlinemiauami/src/main/java/com/gruposiete/hotlinemiauami/HotlinemiauamiApplication.java
@@ -5,9 +5,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @SpringBootApplication
-public class HotlinemiauamiApplication {
+@EnableWebSocket
+public class HotlinemiauamiApplication implements WebSocketConfigurer {
+
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(echoHandler(), "/echo")
+				.setAllowedOrigins("*");
+	}
+
+	@Bean
+	public WebSocketEchoHandler echoHandler() {
+		return new WebSocketEchoHandler();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HotlinemiauamiApplication.class, args);
